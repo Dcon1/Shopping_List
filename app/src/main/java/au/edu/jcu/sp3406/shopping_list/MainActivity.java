83e706378ee8ef0b2d938a2c34f1d91a6cfadd32
Danny <danny.connolly@my.jcu.edu.au>
2020-04-29 20:49:10 +1000

    Initial commit

---
+++ app/src/main/java/au/edu/jcu/sp3406/shopping_list/MainActivity.java
@@ -0,0 +1,14 @@
+package au.edu.jcu.sp3406.shopping_list;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
