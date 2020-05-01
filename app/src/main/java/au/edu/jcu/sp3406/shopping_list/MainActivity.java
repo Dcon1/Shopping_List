f01609432676042acc22a14937e605711437bdc7
Danny <danny.connolly@my.jcu.edu.au>
2020-05-01 18:18:24 +1000

    Final Commit

--- app/src/main/java/au/edu/jcu/sp3406/shopping_list/MainActivity.java
+++ app/src/main/java/au/edu/jcu/sp3406/shopping_list/MainActivity.java
@@ -1,15 +1,12 @@
 package au.edu.jcu.sp3406.shopping_list;
-
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,38 +18,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
-        Bundle extras = getIntent().getExtras();
+        list = findViewById(R.id.list);
+        list.setAdapter(adapter);
 
-        if (extras != null) {
-            String item1 = extras.getString("item");
-            list = findViewById(R.id.list);
-            if (savedInstanceState != null) {
-                String savedState = savedInstanceState.getString("savedState");
-                adapter.addAll(savedState);
-            }
-            adapter.add(item1);
-            adapter.addAll("test");
-            list.setAdapter(adapter);
+            list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    //Toast.makeText(this, "Please enter an item!", Toast.LENGTH_SHORT).show();
+                    String itemClicked = adapter.getItem(position);
+                    adapter.remove(itemClicked);
+                    adapter.notifyDataSetChanged();
+                }
+            });
+
+        }
+        public void add (View view){
+            Intent intent = new Intent(this, AddToList.class);
+            startActivityForResult(intent, 0);
         }
 
-//        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                String item = adapter.getItem(position);
-//                adapter.remove(item);
-//                //Toast.makeText(this, "deleted", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-    }
-
     @Override
-    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
-        super.onSaveInstanceState(outState, outPersistentState);
-        //outState.getStringArray(String.valueOf(adapter));
-    }
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 0){
+            if(resultCode == Activity.RESULT_OK){
+                assert data != null;
+                String text = data.getStringExtra("item");
+                adapter.add(text);
 
-    public void add(View view) {
-        Intent intent = new Intent(this, AddToList.class);
-        startActivity(intent);
+
+            }
+        }
     }
 }
